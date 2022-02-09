635a8bd886e2ac9ca7d35aa2118317fc43a9f081
hemnaath <hemnaathrsurya@gmail.com>
2022-02-09 14:18:35 +0530

    final project

--- src/main/webapp/assets/js/adminDashboard.js
+++ src/main/webapp/assets/js/adminDashboard.js
@@ -1,25 +1,23 @@
 function addRoom()
 {
-	var room=document.getElementById("addRoom");
-	room.style.display="flex";
-	var room=document.getElementById("editRoom");
-	room.style.display="none";
-	var room=document.getElementById("deleteRoom");
-	room.style.display="none";
-	var room=document.getElementById("addWeddingHall");
-	room.style.display="none";
-	var room=document.getElementById("editWeddingHall");
-	room.style.display="none";
-	var room=document.getElementById("cancelWeddingHall");
-	room.style.display="none";
-	var room=document.getElementById("addMeetingHall");
-	room.style.display="none";
-	var room=document.getElementById("editMeetingHall");
-	room.style.display="none";
-	var room=document.getElementById("cancelMeetingHall");
-	room.style.display="none";
-	var room=document.getElementById("listAll");
-	room.style.display="none";
+	var addRoom=document.getElementById("addRoom");
+	addRoom.style.display="flex";
+	var editRoom=document.getElementById("editRoom");
+	editRoom.style.display="none";
+	var deleteRoom=document.getElementById("deleteRoom");
+	deleteRoom.style.display="none";
+	var addWeddingHall=document.getElementById("addWeddingHall");
+	addWeddingHall.style.display="none";
+	var editWeddingHall=document.getElementById("editWeddingHall");
+	editWeddingHall.style.display="none";
+	var cancelWeddingHall=document.getElementById("cancelWeddingHall");
+	cancelWeddingHall.style.display="none";
+	var addMeetingHall=document.getElementById("addMeetingHall");
+	addMeetingHall.style.display="none";
+	var editMeetingHall=document.getElementById("editMeetingHall");
+	editMeetingHall.style.display="none";
+	var cancelMeetingHall=document.getElementById("cancelMeetingHall");
+	cancelMeetingHall.style.display="none";
 }
 function editRoom()
 {
